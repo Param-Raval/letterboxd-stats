6192cd35d85c0fe36d028b7a6bdd3ecd6a6e5554
Param Raval <param005raval@gmail.com>
2022-03-27 14:40:52 +0530

    heroku changes

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+echo "\
+[theme]\n\
+primaryColor="#f7f6f6"\n\
+backgroundColor="#101010"\n\
+secondaryBackgroundColor="#ffffff"\n\
+textColor="#b9babd"\n\
+\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
