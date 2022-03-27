36ee4b1aa4cd501d658fd651de5c710b838adc4d
Param Raval <param005raval@gmail.com>
2022-03-27 11:36:08 +0530

    minor updates

--- app.py
+++ app.py
@@ -124,38 +124,38 @@ async def get_film_main(URLS, meta_data_dict):
     return result_pages
 
 
-def get_meta_dict(ratings_df):
+def get_meta_dict(ratings_data):
     this_month_name = datetime.strptime(f"{datetime.today().month}", "%m").strftime('%B')
     this_month = f"{this_month_name},{datetime.today().year}"
     this_year = f"{datetime.today().year}"
     last_year = f"{int(this_year)-1}"
 
-    ratings_df['rated_date_year']=ratings_df['rated_date'].map(lambda x: f"{x.year}")
-    ratings_df['rated_date_month']=ratings_df['rated_date'].map(lambda x: datetime.strptime(f"{x.month}", "%m").strftime('%B'))
-    ratings_df['rated_date_time_day']=ratings_df['rated_date'].map(lambda x: "Morning" if x.hour<16 else("Evening" if x.hour >= 16 and x.hour<20 else "Night"))
+    ratings_data['rated_date_year']=ratings_data['rated_date'].map(lambda x: f"{x.year}")
+    ratings_data['rated_date_month']=ratings_data['rated_date'].map(lambda x: datetime.strptime(f"{x.month}", "%m").strftime('%B'))
+    ratings_data['rated_date_time_day']=ratings_data['rated_date'].map(lambda x: "Morning" if x.hour<16 else("Evening" if x.hour >= 16 and x.hour<20 else "Night"))
 
-    nfilms_month_wise = ratings_df['rating'].groupby(ratings_df['rated_date'].map(lambda x: str(datetime.strptime(f"{x.month}", "%m").strftime('%B'))+f",{x.year}")).count().reset_index()
+    nfilms_month_wise = ratings_data['rating'].groupby(ratings_data['rated_date'].map(lambda x: str(datetime.strptime(f"{x.month}", "%m").strftime('%B'))+f",{x.year}")).count().reset_index()
     nfilms_this_month = nfilms_month_wise[nfilms_month_wise['rated_date']==this_month]['rating'].values.tolist()
     nfilms_this_month = nfilms_this_month[0] if len(nfilms_this_month)>0 else 0
 
-    nfilms_year_wise = ratings_df['rating'].groupby(ratings_df['rated_date'].map(lambda x: f"{x.year}")).count().reset_index()
+    nfilms_year_wise = ratings_data['rating'].groupby(ratings_data['rated_date'].map(lambda x: f"{x.year}")).count().reset_index()
     nfilms_this_year = nfilms_year_wise[nfilms_year_wise['rated_date']==this_year]['rating'].values.tolist()
     nfilms_this_year = nfilms_this_year[0] if len(nfilms_this_year)>0 else 0
 
     nfilms_most_year, nfilms_most_year_count = nfilms_year_wise.sort_values(by=['rating']).values.tolist()[-1]
 
-    last_year_month_stats = ratings_df['film_name'][ratings_df['rated_date_year']==last_year].groupby(ratings_df['rated_date_month']).count().reset_index().sort_values(by=['film_name']).values.tolist()
+    last_year_month_stats = ratings_data['film_name'][ratings_data['rated_date_year']==last_year].groupby(ratings_data['rated_date_month']).count().reset_index().sort_values(by=['film_name']).values.tolist()
     
     nfilms_last_year_most_month, nfilms_last_year_most_month_count = last_year_month_stats[-1] if len(last_year_month_stats)>0 else "None", 0
 
-    last_year_month_stats = ratings_df['rating'][ratings_df['rated_date_year']==last_year].groupby(ratings_df['rated_date_month']).mean().reset_index().sort_values(by=['rating']).values.tolist()
+    last_year_month_stats = ratings_data['rating'][ratings_data['rated_date_year']==last_year].groupby(ratings_data['rated_date_month']).mean().reset_index().sort_values(by=['rating']).values.tolist()
     nfilms_last_year_most_rated_month, nfilms_last_year_most_rated_month_val = last_year_month_stats if len(last_year_month_stats)>0 else "None", 0
     nfilms_last_year_most_rated_month_val=round(nfilms_last_year_most_rated_month_val,1)
 
-    nfilms_time_of_day = ratings_df['film_name'].groupby(ratings_df['rated_date_time_day']).count().reset_index().sort_values(by=['film_name'])['rated_date_time_day'].values[-1]
+    nfilms_time_of_day = ratings_data['film_name'].groupby(ratings_data['rated_date_time_day']).count().reset_index().sort_values(by=['film_name'])['rated_date_time_day'].values[-1]
 
-    first_film_name, first_film_rating = ratings_df.sort_values(by=['rated_date'])[['film_name', 'rating']].values.tolist()[0]
-    latest_film_name, latest_film_rating = ratings_df.sort_values(by=['rated_date'])[['film_name', 'rating']].values.tolist()[-1]
+    first_film_name, first_film_rating = ratings_data.sort_values(by=['rated_date'])[['film_name', 'rating']].values.tolist()[0]
+    latest_film_name, latest_film_rating = ratings_data.sort_values(by=['rated_date'])[['film_name', 'rating']].values.tolist()[-1]
 
     d = dict_of(nfilms_this_month, nfilms_this_year, nfilms_last_year_most_month,
                 nfilms_last_year_most_month_count,
@@ -165,9 +165,9 @@ def get_meta_dict(ratings_df):
                 first_film_name, first_film_rating,
                 latest_film_name, latest_film_rating,
                 nfilms_most_year, nfilms_most_year_count)
-    ratings_df=ratings_df.drop('rated_date_year', axis=1)
-    ratings_df=ratings_df.drop('rated_date_month', axis=1)
-    ratings_df=ratings_df.drop('rated_date_time_day', axis=1)
+    ratings_data=ratings_data.drop('rated_date_year', axis=1)
+    ratings_data=ratings_data.drop('rated_date_month', axis=1)
+    ratings_data=ratings_data.drop('rated_date_time_day', axis=1)
     return d
 
 
@@ -504,6 +504,11 @@ def get_film_df(user_name):
 
     #all ratings of the user
     ratings_df=ratings_df.drop('rated_date', axis=1)
+    
+    candidates=['rated_date_year','rated_date_month','rated_date_time_day']
+    drop_columns = [x for x in candidates if x in ratings_df.columns]
+    if len(drop_columns)>0:
+        ratings_df=ratings_df.drop(drop_columns, axis=1)
 
     #only newly fetched film data
     column_list = ['lbxd_link', 'year', 'director', 'avg_rating', 'countries', 'langs', 'genres', 'themes', 'top_cast']
