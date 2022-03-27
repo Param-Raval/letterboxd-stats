68f1215840792351f6d3ce12326d5c77baec35c1
Param Raval <param005raval@gmail.com>
2022-03-27 14:55:24 +0530

    heroku changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "\
 [theme]\n\
-primaryColor="#f7f6f6"\n\
-backgroundColor="#101010"\n\
-secondaryBackgroundColor="#ffffff"\n\
-textColor="#b9babd"\n\
+primaryColor=\"#f7f6f6\"\n\
+backgroundColor=\"#101010\"\n\
+secondaryBackgroundColor=\"#ffffff\"\n\
+textColor=\"#b9babd\"\n\
 \n\
 [server]\n\
 headless = true\n\
